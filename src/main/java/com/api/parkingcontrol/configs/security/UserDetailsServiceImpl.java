caa30c51001899ec7a208c7d02ba710eace97961
Renato Ganske <renato.j@lyncas.net>
2023-01-19 11:52:08 -0300

    feat: Implementada a classe User DetailsServiceImpl

---
+++ src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.api.parkingcontrol.configs.security;
+
+import com.api.parkingcontrol.models.UserModel;
+import com.api.parkingcontrol.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    final UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = repository.findByUsername(username).
+                orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username))
+        return userModel;
+    }
+
+}
